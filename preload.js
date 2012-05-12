8812c2757fe09330668b26b9d734da6555cd6495
Suman Jandhyala <sumanvyj@gmail.com>
2012-05-12 12:47:58 -0700

    trans.js complete, preloading function complete

---
+++ preload.js
@@ -0,0 +1,41 @@
+(function() {
+    var sources = [];
+
+    var callback = function() {
+        sway();
+    };
+
+    var loadNextScript = function() {
+        if (sources.length > 0) {
+            var script = document.createElement('script');
+            script.src = sources.shift();
+            document.body.appendChild(script);
+
+            var done = false;
+            script.onload = script.onreadystatechange = function() {
+                if (!done
+                        && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+                    done = true;
+
+                    // Handle memory leak in IE
+                    script.onload = script.onreadystatechange = null;
+
+                    loadNextScript();
+                }
+            }
+        } else {
+            callback();
+        }
+    }
+    
+    if (typeof (jQuery) == 'undefined') {
+        sources.push('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
+    }
+
+    if (typeof (sway) == 'undefined') {
+        sources.push('transform.min.js');
+        sources.push('trans.js');
+    }
+
+    loadNextScript();
+})();
